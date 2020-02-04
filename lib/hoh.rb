4cd8b96367ae347a32845b6c6f196e70e3953c46
Johnson Kow <jkow95@gmail.com>
2020-02-04 20:39:53 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,24 +13,44 @@
 #     }
 #   }
 # }
-# 
+ 
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+ 
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+ 
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
 # }
 
 def naming_system
+  
+  hash = {
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+     }
+   },
+   label: "Order",
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   },
+   label: "Species",
+   sub_category: nil
+ }
+ 
+ hash
+  
   # Remember:
   #  Kingdom
   #  Phylum
